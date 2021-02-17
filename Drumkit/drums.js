f3c42fa7bf0eca78aaf471129e3a24577e411551
lilianarosas <idkpanicking@gmail.com>
2021-02-16 18:20:59 -0600

    added files

---
+++ Drumkit/drums.js
@@ -0,0 +1,7 @@
+var whatkey = window.addEventListener('keydown', playSound(keyitem))
+
+
+function playSound(keyitem) {
+    var keys=["A","S","D","F","G","H","J","K","L"];
+    document.getElementById()
+}
