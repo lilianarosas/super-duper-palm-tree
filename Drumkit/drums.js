538eae776de259418c4471fdc8499b2fb9c77d04
lilianarosas <idkpanicking@gmail.com>
2021-02-16 19:25:59 -0600

    Update drums.js

--- Drumkit/drums.js
+++ Drumkit/drums.js
@@ -1,7 +1,18 @@
-var whatkey = window.addEventListener('keydown', playSound(keyitem))
+function removeTransition(keyitem) {
+    if (keyitem.propertyName !== 'transform') return;
+    keyitem.target.classList.remove('playing');
+  }
 
+  function playSound(keyitem) {
+    const audio = document.querySelector(`audio[data-key="${keyitem.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${keyitem.keyCode}"]`);
+    if (!audio) return;
 
-function playSound(keyitem) {
-    var keys=["A","S","D","F","G","H","J","K","L"];
-    document.getElementById()
-}
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+  }
+
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
